13f021fb8f2ee7ee1a74f6397a9d718519ecf1c4
Sam Bunting <sambunting2008@live.co.uk>
2016-06-22 18:01:44 +0100

    Added a 'Why' and 'Reviews' section

--- index.php
+++ index.php
@@ -5,55 +5,8 @@
 		<title>Document</title>
 		
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
-		<style type="text/css">
-			h1, h2, h3, h4, h5, h6, p {
-				margin: 0;
-				padding: 0;
-			}
-	
-			h1#title {
-				font-size: 4em;
-			}
-
-			#title-block {
-				text-align: center;
-				margin-top: 50px;
-			}
-			
-			#title-block h1 {
-				color: #0066cc;
-			}
-			
-			#browser-screenshots {
-				padding-top: 70px;
-				width: 75%;
-				margin: 0 auto;
-			}
-
-			#browser-screenshots img {
-				width: 50%;
-				float: left;
-				margin-top: 50px;
-			}
-
-			#browser-screenshots::after {
-				content: '';
-				display: block;
-				clear: both;
-			}
-
-			#browser-screenshots img.main {
-				width: 60%;
-				margin-left: -30%;
-				margin-right: -50%;
-				margin-top: -10px;
-				position: relative;
-			}
-
-			p#download-text {
-				text-align: center;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="font-awesome-4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
 	<body>
 		<div id="title-block">
@@ -68,5 +21,43 @@
 		</div>	
 
 		<p id="download-text"><br><br><a href="https://github.com/sambunting/BuntingBrowser/archive/master.zip">DOWNLOAD NOW!!</a><br><br><small>I'll make a proper download button soon. Promise.</small></p>
+	
+		<div id="section-why" class="section container">
+			<h2>Why BuntingBrowser?</h2>
+
+			<div class="why-card">
+				<i class="fa fa-5x fa-thumbs-o-up" aria-hidden="true"></i>
+				<h3>Its Simple</h3>
+				<p>You don't need a browser with all of these plug in malarkey and password managers, BuntingBrowser eliminates them with 0% support!</p>
+			</div>
+
+			<div class="why-card">
+				<i class="fa fa-5x fa-arrow-up" aria-hidden="true"></i>
+				<h3>Its Lightweight</h3>
+				<p>This piece of software doesn't take up 99% of your 4TB hard drive. I would be concerned if any software on there does this anyway...</p>
+			</div>
+
+			<div class="why-card">
+				<i class="fa fa-5x fa-hdd-o" aria-hidden="true"></i>
+				<h3>Doesn't eat RAM</h3>
+				<p>This miracle piece of software doesn't fancy RAM for breakfast, lunch and dinner. Unlike other browsers. <span>*cough* Google Chrome *cough*</span></p>
+			</div>
+
+			<div class="why-card">
+				<i class="fa fa-5x fa-github" aria-hidden="true"></i>
+				<h3>Its Open Source</h3>
+				<p>Its free, and its open source to anyone who knows the C# language. If you don't, what a time it is to learn!</p>
+			</div>
+		</div>
+
+		<div id="section-feedback" class="section container">
+			<h2>Don't just take my word...</h2>
+			<p class="text-center">Just listen to some of these satisfied users.</p>
+
+			<div class="feedback-card">
+				<p>It's basically Internet Explorer 6, but danker.</p>
+				<span>Brad Cross - Someone.</span>
+			</div>
+		</div>
 	</body>
 </html>
